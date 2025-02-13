90d654d8c90f13171e3ebdb34d4c7667a98e728d
SouaJenni <souajenniofc@gmail.com>
2025-02-13 14:13:17 -0300

    querys teste

--- src/main/java/me/souajenni/DAO/AlunoDAO.java
+++ src/main/java/me/souajenni/DAO/AlunoDAO.java
@@ -5,20 +5,25 @@ import me.souajenni.model.Aluno;
 import java.util.List;
 
 public class AlunoDAO {
-
+    // usar Statement para adicionar os dados com proteção contra injeção SQL
     public boolean inserirAluno(Aluno aluno, int idCurso) {
+        String inserirAluno = "insert into aluno (nome, email, telefone, faltas, Curso_idCurso) Values " +
+                "(\"+aluno.getNome()+\", \"+aluno.getEmail()+\",\"+aluno.getTelefone()\", \"+aluno.getFaltas()\", \"+idCurso+\")";
         return false;
     }
 
     public List<Aluno> listarAlunos() {
+        String listarAlunos = "select * from aluno";
         return null;
     }
 
     public List<Aluno> listarAlunosPorCurso(int idCurso) {
+        String listarPorCurso = "select * from aluno where Curso_idCurso = curso.getIdCurso()";
         return null;
     }
 
     public boolean excluirAluno(String email) {
+        String excluirAluno = "delete * from aluno where email = aluno.getEmail()";
         return false;
     }
 }
--- src/main/java/me/souajenni/DAO/CursoDAO.java
+++ src/main/java/me/souajenni/DAO/CursoDAO.java
@@ -7,18 +7,24 @@ import java.util.List;
 public class CursoDAO {
 
     public List<Curso> listarCursos() {
+        String listarCursos = "select * from curso";
         return null;
     }
 
     public boolean inserirCurso(Curso curso) {
+        String inserirCurso = "insert into curso (nome, preco, cargaHoraria) values " +
+                "(\"+curso.getNome()+\", \"+curso.getPreco()+\",\"+curso.getCargaHoraria()\")";
         return false;
     }
 
     public int  atualizarCurso(Curso curso) {
+        String atualizarCurso = "update curso set nome = \"+curso.getNome()+\", preco = \"+curso.getPreco()+\", " +
+                "cargaHoraria = \"+curso.getCargaHoraria() where id = curso.getId()";
         return 0;
     }
 
     public boolean excluirCurso(Curso curso) {
+        String excluirCurso = "delete * from curso were idCurso = curso.getIdCurso()";
         return false;
     }
 }
